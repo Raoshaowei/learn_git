3724fbc4470c00cdbf189fddbdb92f3a25254584
Rao SHaowei <RSWJX2013@163.com>
2018-02-01 20:36:17 +0800

    Test push github

---
+++ hello_world.py
@@ -0,0 +1,22 @@
+message="Hello, world!"
+print(message)
+
+name="my name is rao shao wei"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name="Rao"
+last_name="shaowei"
+full_name=first_name+" "+last_name.title()
+print("Hello,"+" "+full_name+"!")
+print(last_name)
+
+print("\t"+full_name)
+print(first_name+"\n\t"+last_name)
+
+favorite_language="python "
+print(favorite_language.rstrip())
+
+names=['Rao Shaowei','Zou Ningbo']
+print(names)
